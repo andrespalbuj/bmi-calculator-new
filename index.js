595cf8bb48148a128646c46af368d537907854ae
andrespalbuj <andrespalbuj@hotmail.com>
2026-09-06 23:47:01 +0200

    IMC acabado

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // ¡Buena suerte!
+const alturaInput = document.getElementById("height");
+const pesoInput = document.getElementById("weight");
+const btnElement = document.getElementById("btn");
+const bmiResultInput = document.getElementById("bmi-result");
+const weightConditionInput = document.getElementById("weight-condition");
+
+function calculateBMI() {
+    const heightValue = parseFloat(alturaInput.value);
+    const weightValue = parseFloat(pesoInput.value);
+
+    const heightInMeters = heightValue / 100;
+    const bmiValue = weightValue / (heightInMeters * heightInMeters);
+    bmiResultInput.value = bmiValue.toFixed(2);
+
+    if (bmiValue < 18.5) {
+        weightConditionInput.textContent = "Bajo peso";
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+        weightConditionInput.textContent = "Peso normal";
+    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+        weightConditionInput.textContent = "Sobrepeso";
+    } else {
+        weightConditionInput.textContent = "Obesidad";
+    }
+}
+
+btnElement.addEventListener("click", calculateBMI);
