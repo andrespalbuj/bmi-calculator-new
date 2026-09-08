329ec2c29637983f5ac37df084a4507fee57a9e5
andrespalbuj <andrespalbuj@hotmail.com>
2026-09-08 07:06:27 +0200

    IMC con Anotaciones

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const bmiResultInput = document.getElementById("bmi-result");
 const weightConditionInput = document.getElementById("weight-condition");
 
 function calculateBMI() {
+    //Convertimos los strings a números
     const heightValue = parseFloat(alturaInput.value);
     const weightValue = parseFloat(pesoInput.value);
-
+    //Pasamos la altura de centímetros a metros
     const heightInMeters = heightValue / 100;
+    //Calculamos el IMC
     const bmiValue = weightValue / (heightInMeters * heightInMeters);
+    //Mostramos el resultado en el input correspondiente con dos decimales
     bmiResultInput.value = bmiValue.toFixed(2);
-
+    //Mostramos la condición de peso según el IMC
     if (bmiValue < 18.5) {
         weightConditionInput.textContent = "Bajo peso";
     } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
